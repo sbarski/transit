74f7501c4229f5209ccf03db8c11baa9560836d0
Peter Sbarski <peter.sbarski@gmail.com>
2012-09-18 00:48:36 +1000

    Modifications
    Signed-off-by: Peter Sbarski <peter.sbarski@gmail.com>

--- src/Transit/Controllers/ValuesController.cs
+++ src/Transit/Controllers/ValuesController.cs
@@ -20,7 +20,6 @@ namespace Transit.Controllers
 
 
         // GET api/values
-            [AllowAnonymous]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2", "value3" };
